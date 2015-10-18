82fa7dac85a667247825e6760706bcbc14b6c2dd
elephunk84 <iaincstott@gmail.com>
2015-10-18 20:08:06 +0100

    update

--- resources/python/read_temp_local.py
+++ resources/python/read_temp_local.py
@@ -3,7 +3,8 @@ import os
 import glob
 import time
 import time
-
+import socket
+hostname=socket.gethostname()
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
@@ -28,5 +29,6 @@ def read_temp():
 
 while True:
     print read_temp()
+    print hostname
     time.sleep(1)
 
