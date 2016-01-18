3009e0e350f242ffe55cb722da8401a36ddd0383
elephunk84 <iaincstott@gmail.com>
2016-01-18 19:33:28 +0000

    ""

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 cd /home/pi/GitRepo/centralheating
+echo "Which branch have you been editing testing/main???"
+read version
+echo $version
+if [ $version == "testing"]; then
+     echo "Copying testing to main...."
+     cp default_testing.py default.py;
+fi
+if [ $version == "main"]; then
+    echo "Copying main to testing...."
+    cp default.py default_testing.py;
+fi
 echo "Adding files to git...." 
 git add --all
 echo "git Commit comment ="
