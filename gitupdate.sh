f8d8f5f7a392a919d189ed17e01b6e00f7758d2c
elephunk84 <iaincstott@gmail.com>
2016-01-18 16:31:23 +0000

    "v1.0.3.4"

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /home/pi/GitRepo/centralheating
+echo "Adding files to git...." 
 git add --all
+echo "git Commit comment ="
 read comment
 git commit -m \"$comment\"
+echo "git Push...."
 git push origin master
